385e54aa8f4862de2c6bce075aa242b033128ee3
nh3000 <historicanimations@gmail.com>
2024-12-24 16:29:32 -0600

    date picker

---
+++ config/nhdate.go
@@ -0,0 +1,464 @@
+package config
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/lang"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/theme"
+	"fyne.io/fyne/v2/widget"
+)
+
+func daysInMonth(t time.Time, o int) int {
+	// get first day of the given month, add a month, and go one day back
+	return time.Date(t.Year(), t.Month(), 1, 0, 0, 0, 0, t.Location()).AddDate(0, 1+o, -1).Day()
+}
+
+func firstWeekdayOfMonth(t time.Time) time.Weekday {
+	return time.Date(t.Year(), t.Month(), 1, 0, 0, 0, 0, t.Location()).Weekday()
+}
+
+func lastWeekdayOfMonth(t time.Time) time.Weekday {
+	// get first day of the given month, add a month, and go back one day
+	return time.Date(t.Year(), t.Month(), 1, 0, 0, 0, 0, t.Location()).AddDate(0, 1, -1).Weekday()
+}
+
+func timeJumpMonth(when time.Time, offset int) time.Time {
+	if offset == 0 {
+		return when
+	}
+
+	// adjust day when higher than days in the destination month
+	dstDays := daysInMonth(when, offset)
+	if when.Day() > dstDays {
+		when = time.Date(
+			when.Year(),
+			when.Month(),
+			dstDays,
+			when.Hour(),
+			when.Minute(),
+			when.Second(),
+			when.Nanosecond(),
+			when.Location(),
+		)
+	}
+
+	return when.AddDate(0, offset, 0)
+}
+
+func timeJumpYearMonth(when time.Time, year int, month int) time.Time {
+	dst := time.Date(
+		year,
+		time.Month(month),
+		1,
+		when.Hour(),
+		when.Minute(),
+		when.Second(),
+		when.Nanosecond(),
+		when.Location(),
+	)
+
+	// adjust day when higher than days in the destination month
+	dstDays := daysInMonth(dst, 0)
+	dstDay := when.Day()
+	if dstDay > dstDays {
+		dstDay = dstDays
+	}
+
+	return time.Date(
+		dst.Year(),
+		dst.Month(),
+		dstDay,
+		when.Hour(),
+		when.Minute(),
+		when.Second(),
+		when.Nanosecond(),
+		when.Location(),
+	)
+}
+
+// weeks start on Monday, why is it otherwise called "the weekend"?!
+func adjustWeekday(d time.Weekday, weekStart time.Weekday) int {
+	if weekStart == 0 {
+		return int(d)
+	}
+	return ((7 - int(weekStart)) + int(d)) % 7
+}
+
+func updateGrid(grid *fyne.Container, when time.Time, weekStart time.Weekday, updateWhen func(time.Time), updateSelects func(time.Time), strs *dateTimePickerStrings) {
+	objs := []fyne.CanvasObject{}
+
+	left := fyne.TextAlignLeading
+	bold := fyne.TextStyle{Bold: true}
+	// row of weekdays at the top
+	for n := weekStart; n < 7; n++ {
+		objs = append(objs, widget.NewLabelWithStyle(strs.weekdays[n], left, bold))
+	}
+	for n := 0; n < int(weekStart); n++ {
+		objs = append(objs, widget.NewLabelWithStyle(strs.weekdays[n], left, bold))
+	}
+
+	firstWeekday := adjustWeekday(firstWeekdayOfMonth(when), weekStart)
+	lastWeekday := adjustWeekday(lastWeekdayOfMonth(when), weekStart)
+	days := daysInMonth(when, 0)
+	daysPrevMonth := daysInMonth(when, -1)
+
+	// empty fields for days of the previous month that cut into the first week
+	for n := 1; n <= firstWeekday; n++ {
+		day := daysPrevMonth - firstWeekday + n
+		button := widget.NewButton(fmt.Sprintf("%d", day), func() {
+			when = time.Date(
+				when.Year(),
+				when.Month(),
+				1,
+				when.Hour(),
+				when.Minute(),
+				when.Second(),
+				when.Nanosecond(),
+				when.Location(),
+			).AddDate(0, -1, day-1)
+
+			updateWhen(when)
+			updateSelects(when)
+		})
+		button.Importance = widget.LowImportance
+
+		objs = append(objs, button)
+	}
+
+	var buttons []*widget.Button
+	for n := 1; n <= days; n++ {
+		var button *widget.Button
+		var day = n
+
+		button = widget.NewButton(fmt.Sprintf("%02d", n), func() {
+			when = time.Date(
+				when.Year(),
+				when.Month(),
+				day,
+				when.Hour(),
+				when.Minute(),
+				when.Second(),
+				when.Nanosecond(),
+				when.Location(),
+			)
+			updateWhen(when)
+
+			// reset importance of all buttons
+			for _, b := range buttons {
+				b.Importance = widget.MediumImportance
+			}
+			// only highlight selected day
+			button.Importance = widget.HighImportance
+			grid.Refresh()
+		})
+
+		// initially highlight a given day
+		if n == when.Day() {
+			button.Importance = widget.HighImportance
+		}
+
+		buttons = append(buttons, button)
+
+		objs = append(objs, button)
+	}
+
+	// empty fields for days after the previous month
+	for n := 1; lastWeekday < 7 && n < 7-(lastWeekday%7); n++ {
+		day := n
+		button := widget.NewButton(fmt.Sprintf("%d", day), func() {
+			when = time.Date(
+				when.Year(),
+				when.Month(),
+				1,
+				when.Hour(),
+				when.Minute(),
+				when.Second(),
+				when.Nanosecond(),
+				when.Location(),
+			).AddDate(0, 1, day-1)
+
+			updateWhen(when)
+			updateSelects(when)
+		})
+		button.Importance = widget.LowImportance
+
+		objs = append(objs, button)
+	}
+
+	// add up to another empty row to compensate for months with a high first weekday
+	for n := len(objs); n < 7*7; n++ {
+		objs = append(objs, widget.NewLabel(""))
+	}
+
+	grid.Objects = objs
+	grid.Refresh()
+}
+
+func (dtp *DateTimePicker) findMonth(month string) int {
+	for n := 0; n < len(dtp.strings.months); n++ {
+		if dtp.strings.months[n] == month {
+			return n + 1
+		}
+	}
+	return 0
+}
+
+// custom entry that's a bit wider
+type selectEntry struct {
+	widget.SelectEntry
+}
+
+func newSelectEntry(options []string) *selectEntry {
+	e := &selectEntry{}
+	e.ExtendBaseWidget(e)
+	e.SetOptions(options)
+	return e
+}
+
+func (e *selectEntry) MinSize() fyne.Size {
+	o := e.SelectEntry.MinSize()
+	x := widget.NewLabel("").MinSize()
+	o.Width += x.Width
+	return o
+}
+
+type dateTimePickerStrings struct {
+	months   []string
+	weekdays []string
+	now      string
+	time     string
+}
+
+type DateTimePicker struct {
+	widget.BaseWidget
+	container     *fyne.Container
+	updateSelects func(time.Time)
+	strings       *dateTimePickerStrings
+	OnActioned    func(bool)
+}
+
+func (dtp *DateTimePicker) CreateRenderer() fyne.WidgetRenderer {
+	return widget.NewSimpleRenderer(dtp.container)
+}
+
+func (dtp *DateTimePicker) initStrings() {
+	dtp.strings = &dateTimePickerStrings{}
+	dtp.strings.months = []string{
+		lang.X("datepicker.month.january", "January"),
+		lang.X("datepicker.month.february", "February"),
+		lang.X("datepicker.month.march", "March"),
+		lang.X("datepicker.month.april", "April"),
+		lang.X("datepicker.month.may", "May"),
+		lang.X("datepicker.month.june", "June"),
+		lang.X("datepicker.month.july", "July"),
+		lang.X("datepicker.month.august", "August"),
+		lang.X("datepicker.month.september", "September"),
+		lang.X("datepicker.month.october", "October"),
+		lang.X("datepicker.month.november", "November"),
+		lang.X("datepicker.month.december", "December"),
+	}
+
+	dtp.strings.weekdays = []string{
+		lang.X("datepicker.weekday.sun", "Sun"),
+		lang.X("datepicker.weekday.mon", "Mon"),
+		lang.X("datepicker.weekday.tue", "Tue"),
+		lang.X("datepicker.weekday.wed", "Wed"),
+		lang.X("datepicker.weekday.thu", "Thu"),
+		lang.X("datepicker.weekday.fri", "Fri"),
+		lang.X("datepicker.weekday.sat", "Sat"),
+	}
+
+	dtp.strings.now = lang.X("datepicker.now", "Now")
+	dtp.strings.time = lang.X("datepicker.time", "Time")
+}
+
+func NewDatePicker(when time.Time, weekStart time.Weekday, fn func(time.Time, bool)) *DateTimePicker {
+	dtp := &DateTimePicker{}
+	dtp.ExtendBaseWidget(dtp)
+	dtp.initStrings()
+	dtp.OnActioned = func(ok bool) {
+		fn(when, ok)
+	}
+
+	grid := container.New(layout.NewGridLayoutWithColumns(7))
+
+	updateWhen := func(t time.Time) {
+		when = t
+	}
+
+	monthSelect := widget.NewSelect(dtp.strings.months, func(selected string) {
+		i := dtp.findMonth(selected)
+		if i == 0 {
+			return
+		}
+
+		when = timeJumpYearMonth(when, when.Year(), i)
+
+		updateGrid(grid, when, weekStart, updateWhen, dtp.updateSelects, dtp.strings)
+	})
+	monthSelect.Selected = when.Month().String()
+
+	years := []string{}
+	// inverted years, most recent on top for easy selection
+	for n := when.Year() + 10; n >= when.Year()-100; n-- {
+		years = append(years, fmt.Sprintf("%d", n))
+	}
+	yearSelect := widget.NewSelect(years, func(selected string) {
+		i, err := strconv.ParseInt(selected, 10, 64)
+		if err != nil {
+			return
+		}
+
+		when = timeJumpYearMonth(when, int(i), int(when.Month()))
+
+		updateGrid(grid, when, weekStart, updateWhen, dtp.updateSelects, dtp.strings)
+	})
+	yearSelect.Selected = fmt.Sprintf("%d", when.Year())
+
+	dtp.updateSelects = func(t time.Time) {
+		// directly assign instead of setter methods to avoid multiple updates
+		monthSelect.Selected = t.Month().String()
+		monthSelect.Refresh()
+		yearSelect.Selected = fmt.Sprintf("%d", t.Year())
+		yearSelect.Refresh()
+
+		updateGrid(grid, t, weekStart, updateWhen, dtp.updateSelects, dtp.strings)
+	}
+
+	prevMonthButton := widget.NewButtonWithIcon("", theme.NavigateBackIcon(), func() {
+		when = timeJumpMonth(when, -1)
+
+		dtp.updateSelects(when)
+	})
+
+	nextMonthButton := widget.NewButtonWithIcon("", theme.NavigateNextIcon(), func() {
+		when = timeJumpMonth(when, 1)
+
+		dtp.updateSelects(when)
+	})
+
+	top := container.New(
+		// previous and next button left and right
+		layout.NewBorderLayout(nil, nil, prevMonthButton, nextMonthButton),
+		prevMonthButton,
+		nextMonthButton,
+		// month and year dropdowns centered in the middle
+		container.New(
+			layout.NewHBoxLayout(),
+			layout.NewSpacer(),
+			monthSelect,
+			yearSelect,
+			layout.NewSpacer(),
+		),
+	)
+
+	updateGrid(grid, when, weekStart, updateWhen, dtp.updateSelects, dtp.strings)
+
+	dtp.container = container.New(
+		layout.NewBorderLayout(top, nil, nil, nil),
+		top,
+		grid,
+	)
+
+	return dtp
+}
+
+func NewDateTimePicker(when time.Time, weekStart time.Weekday, fn func(time.Time, bool)) *DateTimePicker {
+	dtp := NewDatePicker(when, weekStart, fn)
+
+	hours := []string{}
+	for n := 0; n <= 23; n++ {
+		hours = append(hours, fmt.Sprintf("%02d", n))
+	}
+	hourInput := newSelectEntry(hours)
+
+	minutes := []string{}
+	for n := 0; n <= 59; n++ {
+		minutes = append(minutes, fmt.Sprintf("%02d", n))
+	}
+	minuteInput := newSelectEntry(minutes)
+
+	controlButtons := container.New(
+		layout.NewHBoxLayout(),
+		widget.NewButton("Now", func() {
+			when = time.Now()
+
+			hourInput.SetText(when.Format("15"))
+			minuteInput.SetText(when.Format("04"))
+
+			dtp.updateSelects(when)
+		}),
+	)
+
+	hourInput.SetText(when.Format("15"))
+	hourInput.OnChanged = func(str string) {
+		t, err := time.Parse("15", str)
+		if err != nil {
+			fyne.LogError("invalid hour", err)
+			return
+		}
+
+		when = time.Date(
+			when.Year(),
+			when.Month(),
+			when.Day(),
+			t.Hour(),
+			when.Minute(),
+			0,
+			0,
+			when.Location(),
+		)
+	}
+
+	minuteInput.SetText(when.Format("04"))
+	minuteInput.OnChanged = func(str string) {
+		i, err := strconv.ParseInt(str, 10, 64)
+		if err != nil {
+			fyne.LogError("failed to parse minte value", err)
+			return
+		}
+		if i < 0 || i > 59 {
+			fyne.LogError("minute value out of range", err)
+			return
+		}
+		when = time.Date(
+			when.Year(),
+			when.Month(),
+			when.Day(),
+			when.Hour(),
+			int(i),
+			0,
+			0,
+			when.Location(),
+		)
+	}
+
+	timeForm := widget.NewForm(
+		widget.NewFormItem("Time",
+			container.NewHBox(hourInput, widget.NewLabel(":"), minuteInput),
+		),
+	)
+
+	bottom := container.New(
+		layout.NewBorderLayout(nil, nil, nil, controlButtons),
+		controlButtons,
+		timeForm,
+	)
+
+	top := dtp.container.Objects[0]
+	grid := dtp.container.Objects[1]
+
+	dtp.container = container.New(
+		layout.NewBorderLayout(top, bottom, nil, nil),
+		top,
+		grid,
+		bottom,
+	)
+
+	return dtp
+}
